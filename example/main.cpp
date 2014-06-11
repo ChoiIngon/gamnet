773e51a989f4df27a4336937934f60545753d6b1
kukuta@gmail.com <kukuta@gmail.com@3e3a75c8-d2d5-7a39-339a-82194e09c94f>
2014-06-11 03:18:49 +0000

    router ntf example

--- example/main.cpp
+++ example/main.cpp
@@ -9,11 +9,13 @@
 #include "TestSession.h"
 #include "Message.h"
 #include "HandlerEcho.h"
+#include <Router/Router.h>
 
 int main()
 {
 	Gamnet::Log::Init("log");
 	Gamnet::Network::Listen<ClientSession>(20000, 1024, 60);
+	Gamnet::Router::Listen("HELLOWORLD");
 	Gamnet::Test::Init<TestSession>(1000, 1, 1);
 	Gamnet::Test::RegisterHandler<TestSession, Msg_CS_Echo_Req, Msg_SC_Echo_Ans>(Test_Echo_Req, Test_Echo_Ans);
 	Gamnet::Test::Run<TestSession>("localhost", 20000);
