f3b0d4b2d45a3ed6f14c7271cd1a9f3b6d16ecfa
kukuta@gmail.com <kukuta@gmail.com@3e3a75c8-d2d5-7a39-339a-82194e09c94f>
2014-06-15 14:10:38 +0000

    add monitoring log database bug fix

--- trunk/Gamnet/Test/Test.h
+++ trunk/Gamnet/Test/Test.h
@@ -11,12 +11,14 @@
 #include "Tester.h"
 #include "Session.h"
 #include "../Library/Singleton.h"
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
 
 namespace Gamnet { namespace Test {
 	template <class SESSION_T>
-	void Init(unsigned int elapsed_time, unsigned int session_count, unsigned int loop_count)
+	void Init(unsigned int interval, unsigned int session_count, unsigned int loop_count, const char* host, int port)
 	{
-		Singleton<Tester<SESSION_T>>().Init(elapsed_time, session_count, loop_count);
+		Singleton<Tester<SESSION_T>>().Init(host, port, interval, session_count, loop_count);
 	}
 
 	template<class SESSION_T, class NTF_T>
@@ -26,16 +28,45 @@ namespace Gamnet { namespace Test {
 	}
 
 	template<class SESSION_T, class REQ_T, class ANS_T>
-	void RegisterHandler(typename Tester<SESSION_T>::SEND_HANDER_TYPE send, typename Tester<SESSION_T>::RECV_HANDLER_TYPE recv)
+	bool RegisterHandler(const std::string& test_name, typename Tester<SESSION_T>::SEND_HANDER_TYPE send, typename Tester<SESSION_T>::RECV_HANDLER_TYPE recv)
 	{
-		Singleton<Tester<SESSION_T>>().RegisterHandler<REQ_T, ANS_T>(send, recv);
+		Singleton<Tester<SESSION_T>>().RegisterHandler<REQ_T, ANS_T>(test_name, send, recv);
+		return true;
 	}
 
 	template<class SESSION_T>
-	void Run(const char* host, int port)
+	void Run()
 	{
-		Singleton<Tester<SESSION_T>>().Run(host, port);
+		Singleton<Tester<SESSION_T>>().Run();
+	}
+
+	template<class SESSION_T>
+	void ReadXml(const char* xml_path)
+	{
+		boost::property_tree::ptree ptree_;
+		boost::property_tree::xml_parser::read_xml(xml_path, ptree_);
+
+		const std::string host = ptree_.get<std::string>("server.test.<xmlattr>.host");
+		uint32_t port = ptree_.get<uint32_t>("server.test.<xmlattr>.port");
+		uint32_t interval = ptree_.get<uint32_t>("server.test.<xmlattr>.interval");
+		uint32_t session_count = ptree_.get<uint32_t>("server.test.<xmlattr>.session_count");
+		uint32_t loop_count = ptree_.get<uint32_t>("server.test.<xmlattr>.loop_count");
+		auto test_case = ptree_.get_child("server.test");
+		for(auto elmt : test_case)
+		{
+			if("message" == elmt.first)
+			{
+				Singleton<Tester<SESSION_T>>().SetTestSequence(elmt.second.get<std::string>("<xmlattr>.name"));
+			}
+		}
+		Singleton<Tester<SESSION_T>>().Init(host.c_str(), port, interval, session_count, loop_count);
 	}
 }}
+
+#define GAMNET_BIND_TEST_HANDLER(session_type, send_msg_type, recv_msg_type, send_func, recv_func) \
+	static bool Test_##send_msg_type##_##send_func = Gamnet::Test::RegisterHandler<session_type, send_msg_type, recv_msg_type>( \
+			#send_msg_type, \
+			&send_func, &recv_func \
+	)
 
 #endif /* TEST_H_ */
--- trunk/Gamnet/Test/Tester.h
+++ trunk/Gamnet/Test/Tester.h
@@ -24,43 +24,58 @@ public :
 	typedef std::function<void(std::shared_ptr<SESSION_T>)> SEND_HANDER_TYPE;
 	typedef std::function<void(std::shared_ptr<SESSION_T>, std::shared_ptr<Network::Packet>)> RECV_HANDLER_TYPE;
 private:
-
+	struct TestRunningState
+	{
+		std::mutex lock_;
+		std::string name_;
+		unsigned long int count_;
+		TestRunningState() : name_(""), count_(0) {}
+		TestRunningState(const TestRunningState& t)
+		{
+			name_ = t.name_;
+			count_ = t.count_;
+		}
+	};
+	std::mutex lock_;
 	std::vector<SEND_HANDER_TYPE> vecSendHandler_;
+	std::vector<TestRunningState> vecTestRunningState_;
+	std::map<std::string, SEND_HANDER_TYPE>	mapSendHandler_;
 	std::map<unsigned int, RECV_HANDLER_TYPE> mapRecvHandler_;
 
 	Pool<SESSION_T, std::mutex, Network::Session::Init> sessionPool_;
 	Timer timer_;
 	Timer printTimer_;
 	ThreadPool threadPool_;
-	uint64_t executeCount_;
 	Log::Logger log_;
-public :
-	unsigned int elapsedTime_;
-	unsigned int sessionCount_;
-	unsigned int loopCount_;
-	boost::asio::ip::tcp::resolver resolver_;
 	boost::asio::ip::tcp::endpoint endpoint_;
+
+	uint64_t executeCount_;
+	uint32_t interval_;
+	uint32_t sessionCount_;
+	uint32_t loopCount_;
+
 public :
 	Tester() :
 		sessionPool_(1024),
 		threadPool_(30),
 		executeCount_(0),
-		elapsedTime_(0),
+		interval_(0),
 		sessionCount_(0),
-		loopCount_(0),
-		resolver_(Singleton<boost::asio::io_service>())
+		loopCount_(0)
 	{
 	}
 	virtual ~Tester()
 	{
 	}
 
-	void Init(unsigned int elapsed_time, unsigned int session_count, unsigned int loop_count)
+	void Init(const char* host, int port, unsigned int interval, unsigned int session_count, unsigned int loop_count)
 	{
-		elapsedTime_ = elapsed_time;
+		interval_ = interval;
 		sessionCount_ = session_count;
 		loopCount_ = loop_count;
 		log_.Init("test", 5);
+		boost::asio::ip::tcp::resolver resolver_(Singleton<boost::asio::io_service>());
+		endpoint_ = *resolver_.resolve({host, Format(port).c_str()});
 	}
 	virtual void OnRecvMsg(std::shared_ptr<Network::Session> session, std::shared_ptr<Network::Packet> packet)
 	{
@@ -75,7 +90,10 @@ public :
 
 		RECV_HANDLER_TYPE& handler = itr->second;
 		handler(test_session, packet);
-
+		{
+			std::lock_guard<std::mutex> lo(vecTestRunningState_[test_session->testSEQ_].lock_);
+			vecTestRunningState_[test_session->testSEQ_].count_--;
+		}
 		test_session->testSEQ_++;
 		if(test_session->testSEQ_ >= vecSendHandler_.size())
 		{
@@ -83,6 +101,10 @@ public :
 			return;
 		}
 		vecSendHandler_[test_session->testSEQ_](test_session);
+		{
+			std::lock_guard<std::mutex> lo(vecTestRunningState_[test_session->testSEQ_].lock_);
+			vecTestRunningState_[test_session->testSEQ_].count_++;
+		}
 	}
 	virtual void OnClose(std::shared_ptr<Network::Session> session)
 	{
@@ -95,6 +117,8 @@ public :
 		if(session->testSEQ_ < vecSendHandler_.size())
 		{
 			vecSendHandler_[session->testSEQ_](session);
+			std::lock_guard<std::mutex> lo(vecTestRunningState_[session->testSEQ_].lock_);
+			vecTestRunningState_[session->testSEQ_].count_++;
 		}
 	}
 
@@ -104,32 +128,44 @@ public :
 		mapRecvHandler_[NTF_T::MSG_ID] = recv;
 	}
 	template<class REQ_T, class ANS_T>
-	void RegisterHandler(SEND_HANDER_TYPE send, RECV_HANDLER_TYPE recv)
+	void RegisterHandler(const std::string& test_name, SEND_HANDER_TYPE send, RECV_HANDLER_TYPE recv)
 	{
-		vecSendHandler_.push_back(send);
+		mapSendHandler_[test_name] = send;
 		RegisterHandler<ANS_T>(recv);
 	}
-
-	void Run(const char* host, int port)
+	void SetTestSequence(const std::string& test_name)
 	{
-		endpoint_ = *resolver_.resolve({host, Format(port).c_str()});
-		if(0 == elapsedTime_)
+		auto itr = mapSendHandler_.find(test_name);
+		if(mapSendHandler_.end() == itr)
 		{
-			throw GAMNET_EXCEPTION("elapsedTime_ should be set", 0);
+			throw Exception(Format("can't find registered test case(test_name:", test_name, ")"));
+		}
+		TestRunningState state;
+		state.name_ = test_name;
+		state.count_ = 0;
+		vecTestRunningState_.push_back(state);
+		vecSendHandler_.push_back(itr->second);
+	}
+
+	void Run()
+	{
+		if(0 == interval_)
+		{
+			throw GAMNET_EXCEPTION("interval_ should be set", 0);
 		}
 		if(0 == sessionCount_)
 		{
 			throw GAMNET_EXCEPTION("sessionCount_ should be set", 0);
 		}
 		Log::Write(GAMNET_INF, "test start...");
-		timer_.SetTimer(elapsedTime_, [this, host, port]() {
+		timer_.SetTimer(interval_, [this]() {
 			for(unsigned int i=0; i<sessionCount_; i++)
 			{
 				if(sessionCount_ <= (unsigned int)(this->sessionPool_.Capacity() - this->sessionPool_.Available()))
 				{
 					break;
 				}
-				threadPool_.PostTask([this, host, port]() {
+				threadPool_.PostTask([this]() {
 					std::shared_ptr<SESSION_T> session = this->sessionPool_.Create();
 					if(NULL == session)
 					{
@@ -147,17 +183,22 @@ public :
 			{
 				this->timer_.Resume();
 			}
-			else
-			{
-				Log::Write(GAMNET_INF, "test finished...");
-			}
 		});
 		printTimer_.SetTimer(3000, [this]() {
 			log_.Write(Log::Logger::LOG_LEVEL_INF, "[Test] execute count : ", this->executeCount_);
 			log_.Write(Log::Logger::LOG_LEVEL_INF, "[Test] running session count : ", this->sessionManager_.Size());
+			for(auto& itr : this->vecTestRunningState_)
+			{
+				unsigned long int count = itr.count_;
+				log_.Write(Log::Logger::LOG_LEVEL_INF, "[Test] running state(name : ", itr.name_, ", count : ", count, ")");
+			}
 			if(this->executeCount_ < loopCount_)
 			{
 				this->printTimer_.Resume();
+			}
+			else
+			{
+				Log::Write(GAMNET_INF, "test finished...");
 			}
 		});
 	}
