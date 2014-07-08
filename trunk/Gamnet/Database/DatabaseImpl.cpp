678381502f338d65ed0e2e6fcc713540fc9b3faa
kukuta@gmail.com <kukuta@gmail.com@3e3a75c8-d2d5-7a39-339a-82194e09c94f>
2014-07-08 08:55:19 +0000

    update database lib

--- trunk/Gamnet/Database/DatabaseImpl.cpp
+++ trunk/Gamnet/Database/DatabaseImpl.cpp
@@ -38,7 +38,7 @@ sql::Connection* DatabaseImpl::ConnectionFactory(const ConnectionInfo& connectio
 		connection_properties["OPT_RECONNECT"] = true;
 		connection_properties["OPT_CONNECT_TIMEOUT"] = 3;
 		connection = driver_->connect(connection_properties);
-		connection->setSchema(connectionInfo.db_.c_str());
+		connection->setSchema(connectionInfo.db_);
 		Log::Write(GAMNET_INF, "database connected(uri:", connectionInfo.uri_, ", id:", connectionInfo.id_, ", db:", connectionInfo.db_,")");
 	}
 	catch (const sql::SQLException& e)
@@ -64,7 +64,7 @@ bool DatabaseImpl::Connect(int db_type, const char* host, int port, const char* 
 		return false;
 	}
 
-	if(false == mapConnectionInfo_.	insert(std::make_pair(db_type, connectionInfo)).second)
+	if(false == mapConnectionInfo_.insert(std::make_pair(db_type, connectionInfo)).second)
 	{
 		errno = ERRNO_DUPLICATE_CONNECIONINFO_ERROR;
 		Log::Write(GAMNET_ERR, "duplicate connection info(db_type:", db_type, ", uri:", connectionInfo.uri_, ", id:", connectionInfo.id_, ", db:", connectionInfo.db_,")");
@@ -90,55 +90,47 @@ bool DatabaseImpl::Connect(int db_type, const char* host, int port, const char* 
 ResultSet DatabaseImpl::Execute(int db_type, const std::string& query, std::function<void(ResultSet)> callback)
 {
 	ResultSet res;
-	errno = ERRNO_SUCCESS;
-
 	auto itr = mapConnectionPool_.find(db_type);
 	if(mapConnectionPool_.end() == itr)
 	{
-		Log::Write(GAMNET_ERR, "can't find database connection(db_type:", db_type, ")");
-		errno = ERRNO_NO_CONNECTIONINFO_ERROR;
-		return res;
+		throw Exception(ERRNO_NO_CONNECTIONINFO_ERROR, "ERR [", __FILE__, ":", __func__, "@" , __LINE__, "] can't find database connection(db_type:", db_type, ")");
 	}
 
-	std::shared_ptr<sql::Connection> connection = itr->second->Create();
+	std::shared_ptr<sql::Connection> connection(itr->second->Create());
 	if(NULL == connection)
 	{
-		Log::Write(GAMNET_ERR, "create Connection object error(db_type:", db_type, ")");
-		errno = ERRNO_ALLOC_OBJECT_ERROR;
-		return res;
-	}
-
-	std::shared_ptr<sql::Statement> stmt = std::shared_ptr<sql::Statement>(connection->createStatement());
-	if(NULL == stmt)
-	{
-		Log::Write(GAMNET_ERR, "create Statement object error(db_type:", db_type, ")");
-		errno = ERRNO_ALLOC_OBJECT_ERROR;
-		return res;
+		throw Exception(ERRNO_ALLOC_OBJECT_ERROR, "ERR [", __FILE__, ":", __func__, "@" , __LINE__, "] create Connection object error(db_type:", db_type, ")");
 	}
 	try {
+		std::shared_ptr<sql::Statement> stmt(connection->createStatement());
+		if(NULL == stmt)
+		{
+			throw Exception(ERRNO_ALLOC_OBJECT_ERROR, "ERR [", __FILE__, ":", __func__, "@" , __LINE__, "] create Statement object error(db_type:", db_type, ")");
+		}
+
 		bool isSelectQuery = stmt->execute(query);
 		if(true == isSelectQuery)
 		{
-			res.resultSet_ = std::shared_ptr<sql::ResultSet>(stmt->getResultSet());
+			res.resultSet_.reset(stmt->getResultSet());
 		}
 		else
 		{
 			res.affectedRowCount_ = stmt->getUpdateCount();
 		}
+		stmt->getMoreResults();
+		callback(res);
 	}
 	catch (sql::SQLException& e)
 	{
-		Log::Write(GAMNET_ERR, "query error(query:", query, ", reason:", e.what(), ", error_code:", e.getErrorCode(), ", state:", e.getSQLState(), ")");
 		errno = ERRNO_EXECUTE_QUERY_ERROR;
 		res.errno_ = e.getErrorCode();
 		if(1046 == e.getErrorCode()) // No database selected
 		{
 			const ConnectionInfo& connectionInfo = mapConnectionInfo_[db_type];
-			connection->setSchema(connectionInfo.db_.c_str());
+			connection->setSchema(connectionInfo.db_);
 		}
+		throw Exception(ERRNO_EXECUTE_QUERY_ERROR, "ERR [", __FILE__, ":", __func__, "@" , __LINE__, "] db execute error(query:", query, ", reason:", e.what(), ", error_code:", e.getErrorCode(), ", state:", e.getSQLState(), ")");
 	}
-	stmt->getMoreResults();
-	callback(res);
 	return res;
 }
 
