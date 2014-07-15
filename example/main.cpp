9e8f1380030ea85ee595a96a78e6ba7261c8ae31
kukuta@gmail.com <kukuta@gmail.com@3e3a75c8-d2d5-7a39-339a-82194e09c94f>
2014-07-15 08:50:41 +0000

    modify build error

--- example/main.cpp
+++ example/main.cpp
@@ -10,8 +10,8 @@ int main()
 {
 	Gamnet::Log::ReadXml("config.xml");
 	Gamnet::Network::Listen<ClientSession>(20000, 1024, 60);
-	Gamnet::Router::Listen("HELLOWORLD", 20005);
-	Gamnet::Router::Connect("localhost", 20005);
+	Gamnet::Router::Listen("HELLOWORLD", 20001);
+	Gamnet::Router::Connect("localhost", 20001);
 	Gamnet::Test::ReadXml<TestSession>("config.xml");
 	Gamnet::Test::Run<TestSession>();
 	Gamnet::Run(20);
