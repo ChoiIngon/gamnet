c44bde7eeb42cf3c2a65ad94a04921e85637fba8
kukuta@gmail.com <kukuta@gmail.com@3e3a75c8-d2d5-7a39-339a-82194e09c94f>
2015-02-26 10:17:58 +0000

    소켓 클로즈 상태에 계속 read 이벤트가 발생하는 버그 방어 코드 추가

--- trunk/Gamnet/Library/Buffer.h
+++ trunk/Gamnet/Library/Buffer.h
@@ -10,9 +10,6 @@
 #include <vector>
 #include <cstring>
 #include "Exception.h"
-#ifdef _DEBUG
-#include <assert.h>
-#endif
 
 namespace Gamnet {
 
@@ -26,119 +23,46 @@ namespace Gamnet {
 				return buffer;
 			}
 		};
-        uint16_t writeCursor_;
-        uint16_t readCursor_;
+        int32_t writeCursor_;
+        int32_t readCursor_;
         uint16_t bufSize_;
         char* buf_;
 
-        Buffer(uint16_t size = 1024) : writeCursor_(0), readCursor_(0), bufSize_(size), buf_(NULL)
-        {
-            buf_ = new char[bufSize_];
-#ifdef _DEBUG
-            assert(NULL != buf_);
-#endif
-            memset(buf_, 0, bufSize_);
-        }
-        virtual ~Buffer()
-        {
-            delete [] buf_;
-        }
+        Buffer(uint16_t size = 1024);
+        virtual ~Buffer();
 
-        void Append(const char* buf, uint16_t size)
-        {
-        	/*
-            if(bufSize_ < size + writeCursor_ - readCursor_)
-            {
-                Resize(size + writeCursor_ - readCursor_);
-            }
-            */
-        	if(Available() < size)
-        	{
-        		throw Exception(ENOMEM, "not enough space(need:", size," bytes, left:", Available(), " bytes)");
-        	}
+        void Append(const char* buf, uint16_t size);
+        /**
+         * remove read byte, actually just move read pointer
+         */
+        void Remove(uint16_t size);
 
-            if(size > bufSize_ - writeCursor_)
-            {
-                RemoveReadBuffer();
-            }
+        /**
+         * return size of bytes which is now using
+         */
+        size_t Size() const;
 
-            std::memcpy(buf_+writeCursor_, buf, size);
-            writeCursor_ += size;
-        }
+        const char* ReadPtr() const;
+        char* WritePtr();
 
-        void Remove(uint16_t size)
-        {
-            (readCursor_ += size) > writeCursor_ ? readCursor_ = writeCursor_ : readCursor_;
-        }
+        /**
+         * return : return available buffer size
+         */
+        size_t Available() const;
+        /**
+		 * return : return maximum available buffer size
+		 */
+        size_t Capacity() const;
 
-        size_t Size() const
-        {
-            return writeCursor_ - readCursor_;
-        }
+        /**
+         * set zero write pointer cursor & read pointer cursor
+         */
+        void Clear();
 
-        const char* ReadPtr() const
-        {
-            return buf_+readCursor_;
-        }
-        char* WritePtr()
-        {
-			RemoveReadBuffer();
-			if(writeCursor_ >= bufSize_)
-			{
-				return NULL;
-			}
-            return buf_+writeCursor_;
-        }
-
-        size_t Available() const
-        {
-            return bufSize_ - (writeCursor_ - readCursor_);
-        }
-
-        size_t Capacity() const
-        {
-            return bufSize_;
-        }
-
-        void Clear()
-        {
-            writeCursor_ = 0;
-            readCursor_ = 0;
-        }
-
-        void Resize(uint16_t size)
-        {
-        	char* oldBuf = buf_;
-        	buf_ = new char[size];
-#ifdef _DEBUG
-			assert(NULL != buf_);
-#endif
-        	bufSize_ = size;
-			memset(buf_, 0, bufSize_);
-			if(0 < writeCursor_-readCursor_)
-			{
-				std::memcpy(buf_, oldBuf+readCursor_, writeCursor_ - readCursor_);
-			}
-
-			writeCursor_ -= readCursor_;
-			readCursor_ = 0;
-			delete [] oldBuf;
-        }
+        void Resize(uint16_t size);
         void Copy(const Buffer& buffer);
     protected :
-        void RemoveReadBuffer()
-        {
-        	if(0 >= readCursor_)
-        	{
-        		return;
-        	}
-        	if(0 < writeCursor_-readCursor_)
-        	{
-        		std::memcpy(buf_, buf_+readCursor_, writeCursor_ - readCursor_);
-        	}
-			writeCursor_ -= readCursor_;
-            readCursor_ = 0;
-        }
+        void RemoveReadBuffer();
     };
 } /* namespace Toolkit */
 #endif
