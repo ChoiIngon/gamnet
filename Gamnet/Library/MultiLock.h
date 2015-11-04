f20b6db005d94574954c5bff2404ddb5d2ae5106
ChoiIngon <kukuta@gmail.com>
2015-11-04 16:30:04 +0900

    make MultiLock to be copiable

--- Gamnet/Library/MultiLock.h
+++ Gamnet/Library/MultiLock.h
@@ -8,38 +8,51 @@
 template <class LOCK>
 class MultiLock
 {
-private :
-    std::set<LOCK*> setLocks;
+    class MultiLockImpl
+    {
+    private :
+        std::set<LOCK*> setLocks;
+    public :
+        template <class... LOCKS>
+        MultiLockImpl(LOCKS&&... locks) {
+            Lock(locks...);
+
+            for(auto lock : setLocks)
+            {
+                lock->lock();
+            }
+        }
+
+        ~MultiLockImpl()
+        {
+            for(auto lock : setLocks)
+            {
+                lock->unlock();
+            }
+        }
+
+    private :
+        template <class T, class... LOCKS>
+        void Lock(T& lock, LOCKS&... locks)
+        {
+            setLocks.insert(&lock);
+            Lock(locks...);
+        }
+        template <class T>
+        void Lock(T& lock)
+        {
+            setLocks.insert(&lock);
+        }
+    };
+    std::shared_ptr<MultiLockImpl> impl_;
 public :
     template <class... LOCKS>
     MultiLock(LOCKS&&... locks) {
-        Lock(locks...);
-
-        for(auto lock : setLocks)
-        {
-            lock->lock();
-        }
+        impl_ = std::shared_ptr<MultiLockImpl>(new MultiLockImpl(locks...));
     }
 
     ~MultiLock()
     {
-        for(auto lock : setLocks)
-        {
-            lock->unlock();
-        }
-    }
-
-private :
-    template <class T, class... LOCKS>
-    void Lock(T& lock, LOCKS&... locks)
-    {
-        setLocks.insert(&lock);
-        Lock(locks...);
-    }
-    template <class T>
-    void Lock(T& lock)
-    {
-        setLocks.insert(&lock);
     }
 };
 
