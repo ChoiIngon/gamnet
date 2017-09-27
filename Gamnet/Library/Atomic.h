3a85728bbd40913f8cc46063f07b15becdb4ba5f
ChoiIngon <kukuta@gmail.com>
2017-09-27 17:24:31 +0900

    [1.0.1] add Atomic<std::shared_ptr<T>>

--- Gamnet/Library/Atomic.h
+++ Gamnet/Library/Atomic.h
@@ -1,10 +1,3 @@
-/*
- * Atomic.h
- *
- *  Created on: Oct 13, 2015
- *      Author: wered
- */
-
 #ifndef __GAMNET_LIB_ATOMIC_H_
 #define __GAMNET_LIB_ATOMIC_H_
 
@@ -15,35 +8,68 @@ namespace Gamnet {
 	template <class T> class AtomicPtr;
 	template <class T>
 	class Atomic {
-	    T obj;
-	    std::recursive_mutex lock;
-		template< class _T > friend class AtomicPtr; 
+		T obj;
+		std::recursive_mutex lock;
+		template< class _T > friend class AtomicPtr;
 
-	public :
+	public:
 		Atomic(const T& t) : obj(t) {}
 		Atomic() {}
 	};
-	
+
 	template <class T>
 	class AtomicPtr {
-	    Atomic<T>* ptr;
-	public :
-	    AtomicPtr(const Atomic<T>& obj) : ptr(const_cast<Atomic<T>*>(&obj)) {
-	        ptr->lock.lock();
-	    }
-	    ~AtomicPtr() {
-	        ptr->lock.unlock();
-	    }
-	    T& operator * () {
-	        return ptr->obj;
-	    }
-	    T* operator -> () {
-	        return &(ptr->obj);
-	    }
-	private :
-	    AtomicPtr(const AtomicPtr&);
-	    AtomicPtr& operator = (const AtomicPtr&);
+		Atomic<T>* ptr;
+	public:
+		AtomicPtr(const Atomic<T>& obj) : ptr(const_cast<Atomic<T>*>(&obj)) {
+			ptr->lock.lock();
+		}
+		~AtomicPtr() {
+			ptr->lock.unlock();
+		}
+		T& operator * () {
+			return ptr->obj;
+		}
+		T* operator -> () {
+			return &(ptr->obj);
+		}
+	private:
+		AtomicPtr(const AtomicPtr&);
+		AtomicPtr& operator = (const AtomicPtr&);
+	};
+
+	template <class T>
+	class Atomic<std::shared_ptr<T>> {
+		std::shared_ptr<T> obj;
+		std::recursive_mutex lock;
+		template< class _T > friend class AtomicPtr;
+	public:
+		Atomic(const std::shared_ptr<T>& t) : obj(t) {}
+		Atomic() {}
+	};
+
+	template <class T>
+	class AtomicPtr<std::shared_ptr<T>> {
+		Atomic<std::shared_ptr<T>>* ptr;
+	public:
+		AtomicPtr(const Atomic<std::shared_ptr<T>>& obj) {
+			ptr = const_cast<Atomic<std::shared_ptr<T>>*>(&obj);
+			ptr->lock.lock();
+		}
+		~AtomicPtr() {
+			ptr->lock.unlock();
+		}
+
+		T& operator * () {
+			return *(ptr->obj);
+		}
+		std::shared_ptr<T> operator -> () {
+			return ptr->obj;
+		}
+	private:
+		AtomicPtr(const AtomicPtr&);
+		AtomicPtr& operator = (const AtomicPtr&);
 	};
 }
-#endif /* UTIL_ATOMIC_H_ */
+#endif 
 
