7847413729c262caad3ea4b0fcdac6f64342b3aa
ChoiIngon <kukuta@gmail.com>
2018-03-19 18:02:29 +0900

    [1.0.2] remove unused file

--- example/server/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lGamnet -lboost_filesystem -lboost_system -lcurl -lpthread
-
