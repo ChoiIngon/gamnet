8c85272089b28c174c58e6a205fc2ac995a76971
kukuta <kukuta@gmail.com>
2018-05-01 16:35:25 +0900

    [1.1.5] delegate
    리턴 타입 void 형 함수에 대한 특수화 클래스 추가

--- Gamnet/Library/Delegate.h
+++ Gamnet/Library/Delegate.h
@@ -63,5 +63,61 @@ namespace Gamnet {
 			funcs.clear();
 		}
 	};
+
+	template <class... ARGS>
+	class Delegate<void(ARGS...)>
+	{
+		std::list<std::function<void(ARGS...)>> funcs; 
+	public :
+		typedef typename std::list<std::function<void(ARGS...)>>::iterator iterator;
+
+		void operator () (const ARGS... args)
+		{
+			for(auto func : funcs)
+			{
+				func(args...);
+			}
+		}		
+	
+		Delegate<void(ARGS...)>& operator += (std::function<void(ARGS...)> const& arg) 
+		{
+			funcs.push_back(arg);
+			return *this;
+		}
+		
+		Delegate<void(ARGS...)>& operator -= (std::function<void(ARGS...)> const& arg)
+		{
+			void (*const* ptr)(ARGS...) = arg.template target<void(*)(ARGS...)>();
+			if(nullptr == ptr)
+			{
+				return *this;
+			}	
+	
+			for(auto itr=funcs.begin(); itr!=funcs.end(); itr++ )
+			{
+				void (*const* delegate_ptr)(ARGS...) = (*itr).template target<void(*)(ARGS...)>();
+				if(nullptr != delegate_ptr && *ptr == *delegate_ptr) 
+				{
+					funcs.erase(itr);
+					return *this;
+				}
+			}
+	
+			return *this;
+		}
+	
+		iterator begin() noexcept
+		{
+			return funcs.begin();
+		}
+		iterator end() noexcept
+		{
+			return funcs.end();
+		}
+		void clear()
+		{
+			funcs.clear();
+		}
+	};
 }
 #endif
