80db5b27f61094edee26abc2fa0e88f76a2c7776
ChoiIngon <kukuta@gmail.com>
2020-03-08 01:13:19 +0900

    사용하지 않는 코드 삭제

--- Gamnet/Library/Atomic.h
+++ Gamnet/Library/Atomic.h
@@ -44,35 +44,6 @@ namespace Gamnet {
 		Atomic(const T& t) : obj(t) {}
 		Atomic() {}
 	};
-
-	template <class T>
-	class AtomicPtr<Atomic<T>>
-	{
-		Atomic<T>* ptr;
-	public:
-		AtomicPtr(const Atomic<T>& obj) : ptr(const_cast<Atomic<T>*>(&obj)) 
-		{
-			ptr->lock.lock();
-		}
-		AtomicPtr(const Atomic<T>* obj) : ptr(const_cast<Atomic<T>*>(obj)) 
-		{
-			ptr->lock.lock();
-		}
-		AtomicPtr(const std::shared_ptr<Atomic<T>>& obj) : ptr(const_cast<Atomic<T>*>(obj.get())) 
-		{
-			ptr->lock.lock();
-		}
-		
-		~AtomicPtr() {
-			ptr->lock.unlock();
-		}
-		T* operator -> () {
-			return &(ptr->obj);
-		}
-	private:
-		AtomicPtr(const AtomicPtr&);
-		AtomicPtr& operator = (const AtomicPtr&);
-	};
 }
 #endif 
 
