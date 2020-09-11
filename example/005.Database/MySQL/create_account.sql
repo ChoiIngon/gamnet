ae884f850c0740edf5758d8397a3ec89d3a35e9c
최인곤 <kukuta@gmail.com>
2020-09-12 00:53:10 +0900

    [Delete] 예제 프로젝트 삭제

--- example/005.Database/MySQL/create_account.sql
+++
@@ -1,7 +0,0 @@
--- create mysql account
-USE mysql;
-CREATE USER 'gamnet'@'%' identified by 'P@ssword!';
-GRANT ALL PRIVILEGES ON *.* to 'gamnet'@'%';
-CREATE USER 'gamnet'@'localhost' identified by 'P@ssword!';
-GRANT ALL PRIVILEGES ON *.* to 'gamnet'@'localhost';
-FLUSH PRIVILEGES;
