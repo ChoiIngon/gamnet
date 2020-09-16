20d8882ce49d6a92ef08f7be3ca7443fe074cc63
최인곤 <kukuta@gmail.com>
2020-09-17 00:16:52 +0900

    [Update] 랜덤 함수 데이터 타입 추가

--- Gamnet/Library/Random.cpp
+++ Gamnet/Library/Random.cpp
@@ -25,13 +25,17 @@ namespace Gamnet {
 		std::uniform_int_distribution<int32_t> dist(min, max);
 		return (int32_t)dist(mt.engine);
 	}
-	/*
+
 	uint32_t Random::Range(uint32_t min, uint32_t max)
 	{
+		if (min > max)
+		{
+			throw GAMNET_EXCEPTION(ErrorCode::UndefinedError, "invalid random range(min:", min, ", max:", max, ")");
+		}
 		std::uniform_int_distribution<uint32_t> dist(min, max);
 		return (uint32_t)dist(mt.engine);
 	}
-	*/
+	
 	int64_t Random::Range(int64_t min, int64_t max)
 	{
 		if (min > max)
@@ -41,13 +45,27 @@ namespace Gamnet {
 		std::uniform_int_distribution<int64_t> dist(min, max);
 		return (int64_t)dist(mt.engine64);
 	}
-	/*
+	
 	uint64_t Random::Range(uint64_t min, uint64_t max)
 	{
+		if (min > max)
+		{
+			throw GAMNET_EXCEPTION(ErrorCode::UndefinedError, "invalid random range(min:", min, ", max:", max, ")");
+		}
 		std::uniform_int_distribution<uint64_t> dist(min, max);
 		return (uint64_t)dist(mt.engine64);
 	}
-	*/
+	
+	float Random::Range(float min, float max)
+	{
+		if (min > max)
+		{
+			throw GAMNET_EXCEPTION(ErrorCode::UndefinedError, "invalid random range(min:", min, ", max:", max, ")");
+		}
+		std::uniform_real_distribution<float> dist(min, max);
+		return (float)dist(mt.engine);
+	}
+
 	double Random::Range(double min, double max)
 	{
 		if (min > max)
