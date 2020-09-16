1e2670886510a3cea3926c7ddbc9844b2e0f695d
최인곤 <kukuta@gmail.com>
2020-09-17 00:16:52 +0900

    [Update] 랜덤 함수 데이터 타입 추가

--- Gamnet/Library/Random.h
+++ Gamnet/Library/Random.h
@@ -23,9 +23,10 @@ namespace Gamnet {
 	public:
 		// get random number from min(include) to max(include)
 		static int32_t Range(int32_t min, int32_t max);
-		//static uint32_t Range(uint32_t min, uint32_t max);
+		static uint32_t Range(uint32_t min, uint32_t max);
 		static int64_t Range(int64_t min, int64_t max);
-		//static uint64_t Range(uint64_t min, uint64_t max);
+		static uint64_t Range(uint64_t min, uint64_t max);
+		static float Range(float min, float max);
 		static double Range(double min, double max);
 	};
 
