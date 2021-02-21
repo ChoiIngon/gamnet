b91e9ac14c645cf39b968a0511372695ddf1fa80
ingon choi <kukuta@gmail.com>
2021-02-22 01:51:49 +0900

    [Update] Coroutine<void> 대응

--- Gamnet/Library/Coroutine.h
+++ Gamnet/Library/Coroutine.h
@@ -62,6 +62,62 @@ namespace Gamnet {
 		std::shared_ptr<Enumerator> enumerator;
 	};
 
+	template <>
+	class Coroutine<void> {
+	public:
+		typedef typename boost::coroutines2::coroutine<void>::pull_type Enumerator; // return
+		typedef typename boost::coroutines2::coroutine<void>::push_type Yield; //  
+
+		Coroutine(std::function<void(Yield&)> f)
+			: enumerator(std::make_shared<Enumerator>(f))
+		{
+		}
+
+		Coroutine(Coroutine const& other)
+			: enumerator(other.enumerator)
+		{
+		}
+
+		Coroutine(Coroutine&& other) noexcept
+		{
+			std::swap(enumerator, other.enumerator);
+		}
+
+		Enumerator& GetEnumerator()
+		{
+			return *enumerator;
+		}
+
+		Coroutine& operator=(Coroutine const& other)
+		{
+			enumerator = other.enumerator;
+			return *this;
+		}
+
+		Coroutine& operator=(Coroutine&& other) noexcept
+		{
+			std::swap(enumerator, other.enumerator);
+			return *this;
+		}
+
+		void operator()()
+		{
+			(*enumerator)();
+		}
+
+		operator bool() const noexcept
+		{
+			return (bool)(*enumerator);
+		}
+
+		bool operator!() const noexcept
+		{
+			return !(bool)(*enumerator);
+		}
+	private:
+		std::shared_ptr<Enumerator> enumerator;
+	};
+
 }
 
 #endif
